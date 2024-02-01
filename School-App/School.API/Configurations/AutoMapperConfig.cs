0a12d7f995fb3ea01773b07dc1a44ad9f1834781
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-01 23:19:03 +0530

    Repository

---
+++ School-App/School.API/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using School.API.Data.DTOs;
+using School.API.Data.Entities;
+
+namespace School.API.Configurations;
+
+public class AutoMapperConfig : Profile
+{
+
+    public AutoMapperConfig()
+    {
+        _ = CreateMap<Course, CourseDto>().ReverseMap();
+
+        _ = CreateMap<Course, CreateCourseDto>().ReverseMap();
+    }
+
+}
