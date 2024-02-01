8abbc99d99af01f66dcdce6f3cf5dec061e7d4d1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-01 23:22:36 +0530

    Code Refactoring

---
+++ School-App/School.API/Endpoints/HelloWorldEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using School.API.Business;
+using School.API.Data.DTOs;
+
+namespace School.API.Endpoints;
+
+public static class HelloWorldEndpoints
+{
+
+    public static void MapHelloWorldEndpoints(this IEndpointRouteBuilder routes)
+    {
+        _ = routes.MapGet(HelloWorldRoutes.Root, () => "Hello Minimal API World from Root !!")
+            .WithTags("Hello World")
+            .WithName("GetFromRoot");
+
+        _ = routes.MapGet(HelloWorldRoutes.HelloWorld, () =>
+        {
+            return ApiResponseDto<string>.Create("Hello Minimal API World from /hw !!");
+        }).WithTags("Hello World")
+        .WithName("GetFromHW");
+
+        _ = routes.MapGet(HelloWorldRoutes.Api, DefaultResponseBusiness.SendDefaultApiEndpointOutput)
+            .WithTags("Hello World")
+            .WithName("GetFromApi");
+
+        _ = routes.MapGet(HelloWorldRoutes.ApiV1, () => DefaultResponseBusiness.SendDefaultApiEndpointV1Output())
+            .WithTags("Hello World")
+            .WithName("GetFromApiV1");
+    }
+
+}
