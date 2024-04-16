0523a9d948b508dd9791c21b825a4d4ec074541a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-16 17:49:50 +0530

    Update Playlist.cs

--- Odyssey.MusicMatcher/Types/Playlist.cs
+++ Odyssey.MusicMatcher/Types/Playlist.cs
@@ -1,11 +1,15 @@
 ﻿namespace Odyssey.MusicMatcher.Types;
 
-public class Playlist
+[GraphQLDescription("A curated collection of tracks designed for a specific activity or mood.")]
+public class Playlist(string id, string name)
 {
+    [GraphQLDescription("The ID for the playlist.")]
     [ID]
-    public string Id { get; }
+    public string Id { get; } = id;
 
-    public string Name { get; set; } = string.Empty;
+    [GraphQLDescription("The name of the playlist.")]
+    public string Name { get; set; } = name;
 
+    [GraphQLDescription("Describes the playlist, what to expect and entices the user to listen.")]
     public string? Description { get; set; }
 }
