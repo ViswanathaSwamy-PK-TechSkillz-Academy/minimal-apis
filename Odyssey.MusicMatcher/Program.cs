125d5d8421b610840b5efbac7295567e17193f92
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-16 07:41:39 +0530

    Swamy/15apr work (#4)
    * Initial Version
    
    * Changes
    
    * Update Program.cs
    
    * Initial Version

---
+++ Odyssey.MusicMatcher/Program.cs
@@ -0,0 +1,35 @@
+using Odyssey.MusicMatcher.Types;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Reference: https://www.apollographql.com/tutorials/intro-hotchocolate/05-apollo-explorer
+// Reference: https://graphql.org/learn/
+
+builder.Services.AddGraphQLServer()
+    .AddQueryType<Query>();
+
+builder.Services
+   .AddCors(options =>
+   {
+       options.AddDefaultPolicy(builder =>
+       {
+           builder
+               .WithOrigins("https://studio.apollographql.com")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+       });
+   });
+
+var app = builder.Build();
+
+app.MapGet("/api", () => "Hello World!");
+
+app.UseCors();
+
+// /graphql
+app.MapGraphQL();
+
+app.Run();
+
+
+// 1.annotation - based, 2. code - first, and 3. schema-first.
---
+++ Odyssey.MusicMatcher/Types/Query.cs
@@ -0,0 +1,8 @@
+﻿namespace Odyssey.MusicMatcher.Types;
+
+public class Query
+{
+    // where Query resolver functions will go
+
+    public string Hello() => "Hello world";
+}
