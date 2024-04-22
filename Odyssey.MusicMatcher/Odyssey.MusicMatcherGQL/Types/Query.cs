379c30579e9cb70a74bfef0b982e198b57794774
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-22 14:31:42 +0530

    Changes

--- Odyssey.MusicMatcher/Odyssey.MusicMatcherGQL/Types/Query.cs
+++ Odyssey.MusicMatcher/Odyssey.MusicMatcherGQL/Types/Query.cs
@@ -1,16 +1,15 @@
 ﻿namespace Odyssey.MusicMatcherGQL.Types;
 
+using Microsoft.AspNetCore.Mvc;
 using SpotifyWeb;
 
 public class Query
 {
     [GraphQLDescription("Playlists hand-picked to be featured to all users.")]
-    public async Task<List<Playlist>> FeaturedPlaylists(SpotifyService spotifyService)
+    public async Task<List<Playlist>> FeaturedPlaylists([FromServices] SpotifyService spotifyService)
     {
         var response = await spotifyService.GetFeaturedPlaylistsAsync();
-        // var items = response.Playlists.Items;
-        // var playlists = items.Select(item => new Playlist(item));
-        // return playlists.ToList();
+
         return response.Playlists.Items.Select(item => new Playlist(item)).ToList();
     }
 }
