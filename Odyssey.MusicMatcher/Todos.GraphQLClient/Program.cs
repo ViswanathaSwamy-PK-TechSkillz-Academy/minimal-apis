cba7aa49fd2519c2e684372633827889e447dd7b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-23 18:51:49 +0530

    Changes

--- Odyssey.MusicMatcher/Todos.GraphQLClient/Program.cs
+++ Odyssey.MusicMatcher/Todos.GraphQLClient/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+
+var graphQlEndPoint = "https://apim-micro-services-dev.azure-api.net/graphql";
+
+var options = new GraphQLHttpClientOptions
+{
+    EndPoint = new Uri(graphQlEndPoint),
+    MediaType = "application/json",
+};
+
+var httpClient = new HttpClient();
+httpClient.BaseAddress = new Uri(graphQlEndPoint);
+httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "c3517062ef2c42b8bc94f728d597c7dc");
+
+using var graphQlClient = new GraphQLHttpClient(options, new NewtonsoftJsonSerializer(), httpClient);
+
+// Define your GraphQL query
+var query = @"
+    query TodoItems {
+        todoItems {
+            id
+            title
+            completed
+            userId
+        }
+    }
+";
+
+// Create a GraphQL request object
+var request = new GraphQLRequest
+{
+    Query = query
+};
+
+// Send the GraphQL request using the client
+var response = await graphQlClient.SendQueryAsync<dynamic>(request);
+
+Console.WriteLine(response.Data.todoItems);
