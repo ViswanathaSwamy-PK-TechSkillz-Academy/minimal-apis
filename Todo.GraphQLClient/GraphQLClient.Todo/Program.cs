cd9fce90c3b95a44a3f2e922503805241d61e0c2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-01 19:25:47 +0530

    Changes

--- Todo.GraphQLClient/GraphQLClient.Todo/Program.cs
+++ Todo.GraphQLClient/GraphQLClient.Todo/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GraphQLClient.Todo;
+using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection
+        .AddTodoClient()
+        .ConfigureHttpClient(client =>
+        {
+            client.BaseAddress = new Uri("https://apim-micro-services-dev.azure-api.net/graphql");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "YourSubscriptionKey");
+        });
+
+IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+ITodoClient client = services.GetRequiredService<ITodoClient>();
+
+var result = await client.TodoItems.ExecuteAsync();
+result.EnsureNoErrors();
+
+ForegroundColor = ConsoleColor.DarkCyan;
+
+foreach (var todoItem in result?.Data?.TodoItems!)
+{
+    WriteLine($"Id: {todoItem.Id} \nTitle: {todoItem.Title} \nCompleted: {todoItem.Completed}\n");
+}
+
+ResetColor();
+
+WriteLine("\n\nPress any key ...");
+ReadKey();
