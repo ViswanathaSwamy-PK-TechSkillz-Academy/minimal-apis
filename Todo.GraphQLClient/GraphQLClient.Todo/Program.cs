85c98d474557be6e5d51e0fc3481cd53a392c092
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-07 11:02:29 +0530

    Swamy/01may work (#10)
    * Changes
    
    * Create dotnet-tools.json
    
    * Update dotnet-tools.json
    
    * changes
    
    * Changes
    
    * Changes
    
    * Update ReadMe.md
    
    * Update GraphQLClient.Todo.csproj
    
    * Update ReadMe.md
    
    * Update ReadMe.md
    
    * Update ReadMe.md
    
    * Update ReadMe.md
    
    * Changes
    
    * Changes

---
+++ Todo.GraphQLClient/GraphQLClient.Todo/Program.cs
@@ -0,0 +1,32 @@
+﻿using GraphQLClient.Todo;
+using Microsoft.Extensions.DependencyInjection;
+using StrawberryShake;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection
+        .AddTodoClient()
+        .ConfigureHttpClient(client =>
+        {
+            client.BaseAddress = new Uri("https://apim-micro-services-dev.azure-api.net/graphql");
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "YourSubscriptionKey");
+        });
+
+IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+ITodoClient client = services.GetRequiredService<ITodoClient>();
+
+var result = await client.TodoItems.ExecuteAsync();
+result.EnsureNoErrors();
+
+ForegroundColor = ConsoleColor.DarkCyan;
+
+foreach (var todoItem in result?.Data?.TodoItems!)
+{
+    WriteLine($"Id: {todoItem.Id} \nTitle: {todoItem.Title} \nCompleted: {todoItem.Completed}\n");
+}
+
+ResetColor();
+
+WriteLine("\n\nPress any key ...");
+ReadKey();
---
+++ Todo.GraphQLClient/ReadMe.md
@@ -0,0 +1,18 @@
+# Title
+
+## Sub Title 1
+
+```powershell
+dotnet new sln --name Todo.GraphQLClient
+
+dotnet new console -n GraphQLClient.Todo
+
+dotnet sln add ./GraphQLClient.Todo
+
+dotnet add ./GraphQLClient.Todo package StrawberryShake.Server
+dotnet add ./GraphQLClient.Todo package StrawberryShake.Transport.Http
+
+dotnet graphql init https://apim-micro-services-dev.azure-api.net/graphql -n TodoClient -p ./GraphQLClient.Todo --headers "Ocp-Apim-Subscription-Key=YourKey" --headers "Content-Type=application/json"
+
+dotnet graphql init -n TodoClient -p ./GraphQLClient.Todo --headers "Ocp-Apim-Subscription-Key=YourKey" -x "content-type=application/json" https://apim-micro-services-dev.azure-api.net/graphql
+```
