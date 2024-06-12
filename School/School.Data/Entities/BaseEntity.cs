62d3defc0cb94db7123e8b7c643e46c37b92bfa8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-12 17:03:40 +0530

    Data Project

---
+++ School/School.Data/Entities/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School.Data.Entities;
+
+public class BaseEntity
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public DateTimeOffset CreatedDate { get; set; }
+
+    [Required]
+    public string? CreatedBy { get; set; }
+
+    [Required]
+    public DateTimeOffset ModifiedDate { get; set; }
+
+    [Required]
+    public string? ModifiedBy { get; set; }
+}
