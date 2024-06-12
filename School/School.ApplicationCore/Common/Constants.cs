75a3f519a2343388cbac103bc20bbac12996e814
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-12 17:08:24 +0530

    Application Core

---
+++ School/School.ApplicationCore/Common/Constants.cs
@@ -0,0 +1,16 @@
+﻿namespace School.ApplicationCore.Common;
+
+public static partial class Constants
+{
+    public static class HelloWorldRoutes
+    {
+        public static string Root { get; } = "/";
+
+        public static string HelloWorld { get; } = "/hw";
+
+        public static string Api { get; } = "/api";
+
+        public static string ApiV1 { get; } = "/api/v1";
+    }
+
+}
